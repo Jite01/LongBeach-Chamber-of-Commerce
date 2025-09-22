6a3a09fbbd8ead618726bc1dfbc8d4a51073e356
Jite01 <jamesoyibode968@gmail.com>
2025-09-22 23:29:32 +0100

    landing

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,14 @@ import netlify from '@astrojs/netlify';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
-    vite: {
-        plugins: [tailwindcss()]
-    },
-    integrations: [react()],
-    adapter: netlify()
-});
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  integrations: [react()],
+  adapter: netlify()
+});	
+
+
+
+
